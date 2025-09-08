4f2f9a97ed67c6cb6c417975ff4b0d9ac68bdcd4
金子 勇太 <yuta_kaneko@pj100.biz>
2025-09-08 13:47:24 +0900

    str

--- rust-lesson/src/main.rs
+++ rust-lesson/src/main.rs
@@ -27,4 +27,43 @@ fn main() {
 
     let i2: i64 = 1;
     let i3: i64 = 2;
+
+    let s1 = "Helloこんにちは";
+    let s2 = "World";
+
+    // 文字列リテラルはstaticに確保される
+    // 16バイトで確保される
+    // ptr,lenの2つの情報を持つ
+    // ptrは実データのアドレスを指す
+    // lenは文字列の長さ
+    println!("Stack address of s1 is {:p}", &s1);
+    println!("Stack address of s2 is {:p}", &s2);
+
+    // as_ptr()でstaticに確保されている実データのアドレスを取得できる
+    println!("Static memory address of s1 : {:?}",s1.as_ptr());
+    println!("Static memory address of s2 : {:?}",s2.as_ptr());
+
+    println!("Len of s1 : {}",s1.len());
+    println!("Len of s2 : {}",s2.len());
+
+    // String型は動的に文字列を扱えるのでheapに確保される
+    // 24バイトで確保される
+    // ptr,len,capacityの3つの情報を持つ
+    // capは確保できる容量についての情報
+    // rustが勝手に決めてくれる
+    let mut ss1 = String::from("hello");
+    let mut ss2 = String::from("hello world");
+    println!("Stack address of s1 is {:p}", &ss1);
+    println!("Stack address of s2 is {:p}", &ss2);
+    println!("Heep memory address of s1 is {:p}", ss1.as_ptr());
+    println!("Heep memory address of s2 is {:p}", ss2.as_ptr());
+    println!("Len of s1 : {}, cap : {}",ss1.len(),ss1.capacity());
+    println!("Len of s2 : {}, cap : {}",ss2.len(),ss2.capacity());
+
+    ss1.push_str(" world");
+    println!("After push_str");
+    println!("Stack address of s1 is {:p}", &ss1);
+    println!("Heep memory address of s1 is {:p}", ss1.as_ptr());
+    println!("Len of s1 : {}, cap : {}",ss1.len(),ss1.capacity());  
+
 }
--- rust-lesson/target/debug/deps/rust_lesson-c427dc7f9278ff9d.d
+++ rust-lesson/target/debug/deps/rust_lesson-c427dc7f9278ff9d.d
@@ -1,6 +1,6 @@
-/Users/kanekoyuuta/work_space/rust-lesson/target/debug/deps/rust_lesson-c427dc7f9278ff9d.d: src/main.rs src/vars.rs
+/Users/yuta_kaneko/rust-lesson/rust-lesson/target/debug/deps/rust_lesson-c427dc7f9278ff9d.d: src/main.rs src/vars.rs
 
-/Users/kanekoyuuta/work_space/rust-lesson/target/debug/deps/librust_lesson-c427dc7f9278ff9d.rmeta: src/main.rs src/vars.rs
+/Users/yuta_kaneko/rust-lesson/rust-lesson/target/debug/deps/librust_lesson-c427dc7f9278ff9d.rmeta: src/main.rs src/vars.rs
 
 src/main.rs:
 src/vars.rs:
