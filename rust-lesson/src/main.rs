ffa51d4b240ca44ed3518232cb69fac8896b2de5
金子 勇太 <yuta_kaneko@pj100.biz>
2025-09-11 12:46:18 +0900

    generics

--- rust-lesson/src/main.rs
+++ rust-lesson/src/main.rs
@@ -1,10 +1,12 @@
 // mod vars;
 // mod stack_heep;
-mod ownership;
+// mod ownership;
+mod generics;
 
 fn main() {
     // println!("Hello, world!");
     // vars::run();
     // stack_heep::run();
-    ownership::run();
+    // ownership::run();
+    generics::run();
 }
--- rust-lesson/target/debug/deps/rust_lesson-02927c31874de6fd.d
+++ rust-lesson/target/debug/deps/rust_lesson-02927c31874de6fd.d
@@ -1,6 +1,6 @@
-/Users/yuta_kaneko/rust-lesson/rust-lesson/target/debug/deps/rust_lesson-02927c31874de6fd.d: src/main.rs src/ownership.rs
+/Users/yuta_kaneko/rust-lesson/rust-lesson/target/debug/deps/rust_lesson-02927c31874de6fd.d: src/main.rs src/generics.rs
 
-/Users/yuta_kaneko/rust-lesson/rust-lesson/target/debug/deps/librust_lesson-02927c31874de6fd.rmeta: src/main.rs src/ownership.rs
+/Users/yuta_kaneko/rust-lesson/rust-lesson/target/debug/deps/librust_lesson-02927c31874de6fd.rmeta: src/main.rs src/generics.rs
 
 src/main.rs:
-src/ownership.rs:
+src/generics.rs:
