933d8f4e41ff817aeedba6b01dc5881ed80ab31f
金子勇太 <kanekoyuuta@kawamuranoMacBook-Air.local>
2025-10-04 14:44:53 +0900

    minigrep

---
+++ the-book/minigrep/src/main.rs
@@ -0,0 +1,35 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = parse_config(&args);
+
+    // {}を探しています
+    println!("Searching for {}", config.query);
+    // {}というファイルの中
+    println!("In file {}", config.filename);
+
+    // ファイルが見つかりませんでした
+    let mut f = File::open(config.filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        // ファイルの読み込み中に問題がありました
+        .expect("something went wrong reading the file");
+
+    // テキストは\n{}です
+    println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = &args[1];
+    let filename = &args[2];
+    Config { query, filename }
+}
